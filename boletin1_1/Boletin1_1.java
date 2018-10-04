9e3ea2d94990d6908c40a19303424586ad359634
socamporomani <socamporomani@nautilus10.danielcastelao.org>
2018-10-04 10:06:34 +0200

    inicio

---
+++ boletin1_1/Boletin1_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_1;
+
+/**
+ *
+ * @author socamporomani
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        float area, base=4f, altura=3f;
+        area =(base*altura)/2;
+        System.out.println("el area del rectángulo es de "+area);
+
+    }
+    
+}
